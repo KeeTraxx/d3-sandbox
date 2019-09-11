42cb271d67a6b17048c871c5cbd1d5af7c2e32ef
Khôi Tran <kt@compile.ch>
2019-09-11 16:08:07 +0200

    ready for production

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import 'regenerator-runtime/runtime'
 import './styles.scss'
+import * as topojson from 'topojson'
 import * as d3 from 'd3'
 
-async function main() {
-
-}
-
-main();
+window.d3 = d3;
